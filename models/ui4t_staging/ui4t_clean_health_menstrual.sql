fc4f17fd4bf2d26b9e5712a6727c1d805cd55ad8
pratiksha@projecttech4dev.org <pratiksha@projecttech4dev.org>
2026-08-13 13:46:51 +0000

    Add native menstrual quantity conversion

---
+++ models/ui4t_staging/ui4t_clean_health_menstrual.sql
@@ -0,0 +1,9 @@
+--DBT AUTOMATION has generated this model, please DO NOT EDIT 
+--Please make sure you dont change the model name 
+
+{{ config(materialized='table', schema='ui4t_staging') }}
+WITH cte1 as (
+SELECT "age_raw", "area", "awareness_session_attended", "batch_no", "beneficiary_group", "beneficiary_id", "beneficiary_name", "cluster", "distribution_channel", "distribution_date_text", "distribution_id", "distribution_unit", "district", "donor_or_program", "field_worker_id", "household_id", "partner_ngo", "product_type", "products_distributed", "receipt_acknowledgement", "state", "stockout_reported", "ward", TO_NUMBER("products_distributed", '999') AS products_distributed_numeric FROM {{ref('ui4t_stage_health_menstrual')}})
+-- Final SELECT statement combining the outputs of all CTEs
+SELECT *
+FROM cte1
